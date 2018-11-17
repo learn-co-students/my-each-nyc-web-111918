3154a216b9a03cdf8188529c4945f4cde4fb97ff
Mahmoud Agamy <agamyv@gmail.com>
2018-11-17 00:22:19 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr)
+  new_arr = []
+  if block_given?
+    i = 0
+    while i < arr.length
+      element = yield(arr[i])
+      if element == ""
+        new_arr << yield(arr[i])
+      else
+        new_arr << arr[i]
+      end
+      i+=1
+    end
+  end
+  return new_arr
 end
